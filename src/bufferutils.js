e2f22de2ad8483e4d5dac2fe9e18d8f80988a699
pbca26 <pbca26@gmail.com>
2018-12-12 22:28:51 +0300

    allow max exceeding values (unsafe)

--- src/bufferutils.js
+++ src/bufferutils.js
@@ -5,7 +5,10 @@ var varuint = require('varuint-bitcoin')
 function verifuint (value, max) {
   if (typeof value !== 'number') throw new Error('cannot write a non-number as a number')
   if (value < 0) throw new Error('specified a negative value for writing an unsigned value')
-  if (value > max) throw new Error('RangeError: value out of range')
+  if (value > max) {
+    value = max;
+    // throw new Error('RangeError: value out of range')
+  }
   if (Math.floor(value) !== value) throw new Error('value has a fractional component')
 }
 
